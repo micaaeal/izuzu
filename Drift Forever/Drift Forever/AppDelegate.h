b57179da704cc853863ded57fb7b1440a9e3949d
Adawat <prophecyx@hotmail.com>
2012-10-02 06:54:00 +0700

    Fresh Commit

---
+++ Drift Forever/Drift Forever/AppDelegate.h
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.h
+//  Drift Forever
+//
+//  Created by Adawat Chanchua on 10/2/55 BE.
+//  Copyright __MyCompanyName__ 2555. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "cocos2d.h"
+
+@interface AppController : NSObject <UIApplicationDelegate, CCDirectorDelegate>
+{
+	UIWindow *window_;
+	UINavigationController *navController_;
+
+	CCDirectorIOS	*director_;							// weak ref
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (readonly) UINavigationController *navController;
+@property (readonly) CCDirectorIOS *director;
+
+@end
---
+++ Drift Forever/Drift Forever/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Drift Forever
+//
+//  Created by Adawat Chanchua on 10/2/55 BE.
+//  Copyright __MyCompanyName__ 2555. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
